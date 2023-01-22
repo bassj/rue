918734df5e39002639f1a283e2054e8dda06c364
Jack Bass <jbass2000@gmail.com>
2023-01-21 18:39:19 -0800

    Parse multiple statements at once

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -3,6 +3,7 @@ mod error;
 mod expr;
 mod util;
 mod func;
+mod statement;
 
 use nom::Err;
 
@@ -16,29 +17,20 @@ type InputType<'p> = LocatedSpan<&'p str>;
 type IResult<'p, T> = nom::IResult<InputType<'p>, T, ParseError<'p>>;
 
 /// The main entry point for parsing a rue program
-pub fn parse_source<'s, T: Into<&'s str>>(input: T) -> Result<Vec<ast::Expression>, ParseError<'s>> {
+pub fn parse_source<'s, T: Into<&'s str>>(input: T) -> Result<Vec<ast::Statement>, ParseError<'s>> {
     let src_string = input.into();
     let src = LocatedSpan::new(src_string.as_ref());
 
     let res = nom::combinator::all_consuming(
-        nom::multi::many0(parse_statement)
+        nom::multi::many0(statement::parse_statement)
     )(src);
 
     match res {
-        Ok((_, expr)) => Ok(expr),
+        Ok((_, stmt)) => Ok(stmt),
         Err(e) => match e {
             Err::Failure(e) => Err(e),
             Err::Error(e) => Err(e),
-            _ => Err(ParseError::default()),
+            _ => unimplemented!(),
         },
     }
-}
-
-fn parse_statement(input: InputType) -> IResult<Expression> {
-    let t = nom_preserve::sequence::terminated(
-        nom_preserve::error::blame(expr::parse_expression),
-        nom::branch::alt((nom::character::complete::line_ending, nom::combinator::eof)),
-    )(input);
-
-    t.map(|(i, (expr, _err_stack))| (i, expr))
 }
---
+++ src/parse/statement.rs
@@ -0,0 +1,82 @@
+use crate::ast::Statement;
+use crate::parse::error::ErrorStack;
+use crate::parse::IResult;
+use crate::parse::InputType;
+use crate::parse::util;
+
+pub fn parse_statement(input: InputType) -> IResult<Statement> {
+    let t = nom_preserve::sequence::terminated(
+        nom_preserve::error::blame(nom::branch::alt((
+            nom::combinator::map(super::expr::parse_expression, |(expr, err_stack)| {
+                (Statement::Expression(expr), err_stack)
+            }),
+            parse_variable_declaration,
+        ))),
+        nom::branch::alt((nom::character::complete::line_ending, nom::combinator::eof)),
+    )(input);
+
+    t.map(|(i, (expr, _err_stack))| (i, expr))
+}
+
+fn parse_variable_declaration(input: InputType) -> IResult<(Statement, ErrorStack)> {
+    let (input, (var_name, (var_value, error_stack))) = nom::sequence::preceded(
+        util::ws(nom::bytes::complete::tag("let")), 
+        nom::sequence::separated_pair(
+            super::util::parse_identifier,
+            util::ws(nom::bytes::complete::tag("=")),
+            super::expr::parse_expression,
+        ),  
+    )(input)?;
+
+    let stmt = Statement::VariableDeclaration(var_name, var_value);
+
+    Ok((input, (stmt, error_stack)))
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_parse_variable_declaration() {
+        use crate::ast::Expression;
+        use nom_locate::LocatedSpan;
+
+        let input = LocatedSpan::new("let test = 10");
+        let (input, (stmt, _error_stack)) = parse_variable_declaration(input).unwrap();
+
+        assert_eq!(input.fragment(), &"", "Parser returns correct input");
+
+        assert_eq!(
+            stmt,
+            Statement::VariableDeclaration("test".to_string(), Expression::IntegerLiteral(10)),
+            "Parser parses correct statement"
+        );
+    }
+
+    #[test]
+    fn test_parse_statement_variable_declaration() {
+        use crate::ast::Expression;
+        use nom_locate::LocatedSpan;
+
+        let input = LocatedSpan::new("let test = 10\nlet test2 = 10");
+        let (input, stmt) = parse_statement(input).unwrap();
+        
+        assert_eq!(input.fragment(), &"let test2 = 10", "Parser returns correct input");
+
+        assert_eq!(
+            stmt,
+            Statement::VariableDeclaration("test".to_string(), Expression::IntegerLiteral(10)),
+            "Parser parses correct statement"
+        );
+
+        let (input, stmt) = parse_statement(input).unwrap();
+
+        assert_eq!(input.fragment(), &"", "Parser returns correct input");
+
+        assert_eq!(
+            stmt,
+            Statement::VariableDeclaration("test2".to_string(), Expression::IntegerLiteral(10)),
+            "Parser parses correct statement"
+        );
+    }
+}
